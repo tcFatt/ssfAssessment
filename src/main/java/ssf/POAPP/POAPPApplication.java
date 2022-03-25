2190863bc57d8040df97d552e35ca1d3b8746471
tcFatt <chiunfatt@gmail.com>
2022-03-25 09:56:40 +0800

    first commit

---
+++ src/main/java/ssf/POAPP/POAPPApplication.java
@@ -0,0 +1,13 @@
+package ssf.POAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class POAPPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(POAPPApplication.class, args);
+	}
+
+}
